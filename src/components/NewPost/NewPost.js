f4d1d29176c560f100fa4f0f7991903570587ee7
wawk <white_knight@prodigy.net>
2020-11-12 09:52:26 -0800

    finished with blog post app by adding axios instances

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -19,7 +19,7 @@ class NewPost extends Component {
 
 
         };
-        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        axios.post('/posts', data)
         .then(response =>{
             console.log(response);
         });
